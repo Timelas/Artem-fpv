259dbe668923afe2bd1c384bba38cf85c88a9990
Timelas <lom.a.s@icloud.com>
2023-01-23 14:07:55 +0300

    открытие ссылок в новой вкладке

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
     <footer className="footer">
         <p className="footer__by">&copy; Ломакова А.С. 2022</p>
       <div className="footer__links">
-        <a className="footer__link" href="tel:+79215883019">+7 (931) 706-16-09</a>
-        <a className="footer__link" href="https://www.youtube.com/@abaksha">YouTube</a>
-        <a className="footer__link" href="https://vk.com/abakshaa">VK</a>
+        <a className="footer__link" href="tel:+79215883019" target="_blank" rel="noreferrer">+7 (931) 706-16-09</a>
+        <a className="footer__link" href="https://www.youtube.com/@abaksha" target="_blank" rel="noreferrer">YouTube</a>
+        <a className="footer__link" href="https://vk.com/abakshaa" target="_blank" rel="noreferrer">VK</a>
       </div>
     </footer>
   );
