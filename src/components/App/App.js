71204b1248af8e500113428f6d3375bc3f05cdcc
Timelas <lom.a.s@icloud.com>
2023-01-23 20:07:28 +0300

    настройка редирект

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,89 +1,14 @@
-import './App.css';
 import React from 'react';
-import { Route, Routes, Redirect } from 'react-router-dom';
-import Header from "../Header/Header";
-import Popup from "../Popup/Popup";
-import PopupVideo from "../PopupVideo/PopupVideo";
-import ButtonFix from "../ButtonFix/ButtonFix";
-import Portfolio from "../Portfolio/Portfolio";
-import Information from "../Information/Information";
-import FormBlock from "../FormBlock/FormBlock";
-import Footer from "../Footer/Footer";
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Home from "../Home/Home";
 
 function App() {
-  const [isOpenPopup, setIsOpenPopup] = React.useState(false);
-  const [isOpenVideo, setIsOpenVideo] = React.useState(false);
-  const [isButtonVisible, setIsButtonVisible] = React.useState(true);
-  const [isformAnswer, setIsformAnswer] = React.useState(false);
-  const [isformVisible, setIsformVisible] = React.useState(true);
-  const [isArrowVisible, setIsArrowVisible] = React.useState(true);
-  const [isCard, setIsCard] = React.useState([]);
-
-  function handleOpenPopup () {
-    setIsOpenPopup(true);
-    setIsButtonVisible(false);
-    setIsArrowVisible(false);
-    setIsformVisible(true);
-    setIsformAnswer(false);
-  }
-
-  function handleOpenVideo (card) {
-    setIsOpenVideo(true);
-    setIsButtonVisible(false);
-    setIsArrowVisible(false);
-    setIsCard(card);
-  }
-
-  function handleClosePopup () {
-    setIsOpenPopup(false);
-    setIsButtonVisible(true);
-    setIsArrowVisible(true);
-  }
-
-  function handleCloseVideo () {
-    setIsOpenVideo(false);
-    setIsButtonVisible(true);
-    setIsArrowVisible(true);
-    setIsCard([]);
-  }
-
-  function visibleAnswerForm () {
-    setIsformAnswer(true);
-    setIsformVisible(false);
-  }
 
   return (
-    <div className="App">
       <Routes>
-        <Route path="/" exact>
-          <Header isArrowVisible={isArrowVisible}/>
-          <ButtonFix
-            isButtonVisible={isButtonVisible}
-            onOpenPopup={handleOpenPopup} />
-          <Portfolio
-            onOpenVideo={handleOpenVideo} />
-          <Information />
-          <FormBlock
-            visibleAnswerForm={visibleAnswerForm}
-            visibleForm={isformVisible}
-            onSendForm={isformAnswer} />
-          <Footer />
-          <Popup
-            isOpen={isOpenPopup}
-            visibleAnswerForm={visibleAnswerForm}
-            onClosePopup={handleClosePopup}
-            visibleForm={isformVisible}
-            onSendForm={isformAnswer} />
-          <PopupVideo
-            isOpen={isOpenVideo}
-            card={isCard}
-            onCloseVideo={handleCloseVideo} />
-        </Route>
-        <Route path='*'>
-          <Redirect to="/" />
-        </Route>
+        <Route path="/" element={<Home />} />
+        <Route path='*' element={<Navigate to="/"/>} />
       </Routes>
-    </div>
   );
 }
 
---
+++ src/components/Home/Home.js
@@ -0,0 +1,82 @@
+import '../App/App.css';
+import React from 'react';
+import Header from "../Header/Header";
+import Popup from "../Popup/Popup";
+import PopupVideo from "../PopupVideo/PopupVideo";
+import ButtonFix from "../ButtonFix/ButtonFix";
+import Portfolio from "../Portfolio/Portfolio";
+import Information from "../Information/Information";
+import FormBlock from "../FormBlock/FormBlock";
+import Footer from "../Footer/Footer";
+
+function Home() {
+  const [isOpenPopup, setIsOpenPopup] = React.useState(false);
+  const [isOpenVideo, setIsOpenVideo] = React.useState(false);
+  const [isButtonVisible, setIsButtonVisible] = React.useState(true);
+  const [isformAnswer, setIsformAnswer] = React.useState(false);
+  const [isformVisible, setIsformVisible] = React.useState(true);
+  const [isArrowVisible, setIsArrowVisible] = React.useState(true);
+  const [isCard, setIsCard] = React.useState([]);
+
+  function handleOpenPopup () {
+    setIsOpenPopup(true);
+    setIsButtonVisible(false);
+    setIsArrowVisible(false);
+    setIsformVisible(true);
+    setIsformAnswer(false);
+  }
+
+  function handleOpenVideo (card) {
+    setIsOpenVideo(true);
+    setIsButtonVisible(false);
+    setIsArrowVisible(false);
+    setIsCard(card);
+  }
+
+  function handleClosePopup () {
+    setIsOpenPopup(false);
+    setIsButtonVisible(true);
+    setIsArrowVisible(true);
+  }
+
+  function handleCloseVideo () {
+    setIsOpenVideo(false);
+    setIsButtonVisible(true);
+    setIsArrowVisible(true);
+    setIsCard([]);
+  }
+
+  function visibleAnswerForm () {
+    setIsformAnswer(true);
+    setIsformVisible(false);
+  }
+
+  return (
+    <div className="App">
+      <Header isArrowVisible={isArrowVisible}/>
+      <ButtonFix
+        isButtonVisible={isButtonVisible}
+        onOpenPopup={handleOpenPopup} />
+      <Portfolio
+        onOpenVideo={handleOpenVideo} />
+      <Information />
+      <FormBlock
+        visibleAnswerForm={visibleAnswerForm}
+        visibleForm={isformVisible}
+        onSendForm={isformAnswer} />
+      <Footer />
+      <Popup
+        isOpen={isOpenPopup}
+        visibleAnswerForm={visibleAnswerForm}
+        onClosePopup={handleClosePopup}
+        visibleForm={isformVisible}
+        onSendForm={isformAnswer} />
+      <PopupVideo
+        isOpen={isOpenVideo}
+        card={isCard}
+        onCloseVideo={handleCloseVideo} />
+    </div>
+  );
+}
+
+export default Home;
