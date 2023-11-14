381f765ae8d6ee87ee16bc34e956ac012820f9d6
Timelas <lom.a.s@icloud.com>
2023-11-14 18:05:46 +0300

    test

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,95 +1,72 @@
-import React, {Suspense} from "react";
-import axios from "axios";
-import { useForm } from 'react-hook-form';
-import { ErrorMessage } from "@hookform/error-message";
+import React, {Suspense, useState, useRef} from "react";
 import "./Form.css";
 import Preloader from '../Preloader/Preloader';
 import iconWA from "../../images/WA.png";
 import iconTg from "../../images/Telegram.png";
 
 function Form(props) {
-  // const form = document.forms.form;
-  // const tel = form.elements.phone;
+  const formRef = useRef(null)
   const {closePopup, isButtonCloseVisible, onSendForm, visibleAnswerForm, visibleForm} = props;
-  const { register, formState: { errors, isValid }, handleSubmit, clearErrors, reset } = useForm({
-    defaultValues: {
-      phone: '',
-      name: '',
-      description: ''
-    },
-    mode: "onBlur",
-    criteriaMode: 'all',
-  });
+  const [loading, setLoading] = useState(false);
+  const scriptUrl = "https://script.google.com/macros/s/AKfycbwG2YKxMZ46R1ZqFlgSWuTA1aDSamH8A-e9WJPsOQblrZlrJ0pxYPrEU1xKofkwuTu8/exec"
 
-  function onSubmit(data) {
-    axios.post('https://sheet.best/api/sheets/2a3d941b-6152-41ae-997c-ec67d31a9e8d', data)
-    .then(response => {
-      if (response.status === 200) {
-        visibleAnswerForm();
-      }
+ const onSubmit = (e) =>{
+  e.preventDefault();
+  setLoading(true);
+  const FormNew = new FormData(formRef.current);
+  FormNew.append('Form', 'questions');
+
+  fetch(scriptUrl, {
+    method: 'POST',
+    body: new FormData(formRef.current),
+
+}).then(res => {
+      visibleAnswerForm();
+      setLoading(false);
+      e.target.reset();
     })
-  }
-
-  // function add () {
-  //   return tel.value="+7";
-  // }
+    .catch(err => console.log(err))
+}
 
 
   return (
     <Suspense fallback={<Preloader/>}>
-      <div className="form">
+      <div className="form" id="questions">
         <p className={`form__answer-good form__answer-good_${onSendForm && 'visible'}`}>Ваша заявка отправлена, я свяжусть с вами в ближайшее время!</p>
-        <button className={`form__close form__close_${isButtonCloseVisible && 'visible'}`} onClick={() => {clearErrors(); closePopup(); reset()}}></button>
+        <button className={`form__close form__close_${isButtonCloseVisible && 'visible'}`} onClick={closePopup}></button>
         <div className={`form__container form__container_${visibleForm && 'visible'}`}>
           <h3 className="form__title">Оставьте свой номер и я свяжусь с вами в ближайшее время</h3>
-          <form className="form__inputs" onSubmit={handleSubmit(onSubmit)} action="mailto:alexandra.lomakova@yandex.ru" encType="text/plain" name="form">
+          <form className="form__inputs" onSubmit={onSubmit} encType="text/plain" name="form" ref={formRef}>
             <div className="form__two-inputs">
               <div className="form__block-input">
-                <label htmlFor="phone"></label>
+                <label htmlFor="tel"></label>
                 <input
                   type="tel"
-                  // onClick={add}
-                  {...register('phone', {
-                    required: "Это поле обязательно, введите номер телефона",
-                    pattern: {
-                      value: /^[0-9+-]+$/,
-                      message: "В это поле можно вводить только цифры",
-                    },
-                  }
-                  )}
                   className="form__input form__input_type_phone"
-                  name="phone"
+                  name="Телефон"
                   placeholder="телефон"
                   id="tel"
+                  required
                   ></input>
-              <ErrorMessage
-                errors={errors}
-                name="phone"
-                render={({ message }) => <p className="form__input-error">{message}</p>}
-              />
               </div>
               <div className="form__block-input">
                 <label htmlFor="name"></label>
                 <input
                   type="text"
-                  {...register('name', {
-                    minLength: 2,
-                    maxLength: 14,
-                  })}
                   className="form__input form__input_type_name"
-                  name="name"
-                  placeholder="имя"></input>
-                  {errors.name && <p className="form__input-error"></p>}
+                  name="Имя"
+                  placeholder="имя"
+                  id="name"></input>
               </div>
             </div>
             <label htmlFor="description"></label>
             <textarea
               type="text"
-              {...register('description')}
               className="form__input form__input_type_description"
-              name="description"
-              placeholder="что нужно снять?"></textarea>
-            <button className="form__btn" type="submit" disabled={!isValid} onClick="ym(91663099,'reachGoal','button_otpravit')">Отправить</button>
+              name="Описание"
+              placeholder="что нужно снять?"
+              id="description"></textarea>
+            <button className="form__btn" type="submit" disabled={loading ? true : false}>{loading ? "Отправка..." : "Отправить"}</button>
         </form>
           <div className="form__contacts">
               <div className="form__line"></div>
